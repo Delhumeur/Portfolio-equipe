34f99c30135da40dd2f08e1235391e5f6027a738
joJosh59 <joshua97@hotmail.fr>
2024-01-23 11:31:29 +0100

    review card

--- js/sscript.js
+++ js/sscript.js
@@ -1,1 +1,4 @@
-rere
+https://fastasf.netlify.app/
+https://techcrunch.com/2021/11/14/nigerian-e-health-pharmaceutical-distribution-startup-drugstoc-secures-4-4-million-series-a-funding-embarks-on-expansion-drive/?guccounter=1
+https://www.scoutbar.co/
+https://audiomack.com/
